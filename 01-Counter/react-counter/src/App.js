888df497346950f6018398ebb843e76f4338f194
IntermediaDesigns <intermediadesignsllc@gmail.com>
2023-11-24 18:21:19 -0500

    counter project done

--- 01-Counter/react-counter/src/App.js
+++ 01-Counter/react-counter/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// need to add import: In a React.js app, the useState hook is used to manage state in functional components. It allows you to declare state variables and update them, triggering a re-render of the component when the state changes.
+
+/*Here's a brief description of how to use useState:
+
+Import the Hook:
+
+import React, { useState } from 'react';
+
+Declare State:
+
+const [stateVariable, setStateVariable] = useState(initialValue);
+
+stateVariable: The variable that holds the current state value.
+
+setStateVariable: The function used to update the state.
+
+initialValue: The initial value of the state.*/
+
+import { useState } from "react"
 import './App.css';
 
 function App() {
+  // Add count to function and insert {count} in between h1
+  const [count, setCount] = useState(0);
+
+  const addToCount = () => {
+    if (count < 10) {
+      setCount(count + 1)
+    }
+  }
+
+  //copy add function and make it subtract function
+  const subtractFromCount = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  // add onClick to the buttons
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{count}</h1>
+      <button onClick={subtractFromCount}>-</button>
+      <button onClick={addToCount}>+</button>
     </div>
-  );
+  )
 }
 
 export default App;
